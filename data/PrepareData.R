5add6f8662d5d57b46b070a9b1ab48dd3bef755f
javob <javob@users.noreply.github.com>
2017-10-11 22:22:22 +0300

    v0.2

--- data/PrepareData.R
+++ data/PrepareData.R
@@ -4,8 +4,8 @@
 load(url("https://github.com/javob/BarplotSurvey/raw/master/data/SurveyData.RData"))
 
 # Define question titles 
-dataVars       <- names(lapop[,8:22])
-dataQuestions  <- c("¿dónde se encontraría usted en esta escala (ideológica)?",
+dataVars       = names(lapop[,8:22])
+dataQuestions  = c("¿dónde se encontraría usted en esta escala (ideológica)?",
                     "En su opinión ¿cuál es el problema más grave que está enfrentando el país?",
                     "El Estado guatemalteco, en lugar del sector privado, debería ser el dueño de las empresas e industrias más importantes del país. ¿Hasta qué punto está de acuerdo o en desacuerdo con esta frase?",
                     "El Estado guatemalteco debe implementar políticas firmes para reducir la desigualdad de ingresos entre ricos y pobres. ¿Hasta qué punto está de acuerdo o en desacuerdo con esta frase?",
@@ -20,9 +20,9 @@ dataQuestions  <- c("¿dónde se encontraría usted en esta escala (ideológica)
                     "¿Cree usted que se debe incluir el sistema de justicia indígena en la Constitución de Guatemala?",
                     "¿Quién cree que es el culpable de la mayoría de la violencia que ocurrió durante el conflicto armado, el Ejército o la guerrilla, o los dos igualmente?",
                     "En su opinión, ¿cree que Ríos Montt debería ser condenado por genocidio en contra de los Ixiles?")
-dataQuestions2 <- sapply(dataQuestions, function(x) paste(strwrap(x, 50), collapse = "\n"))
-names(dataQuestions2) <- NULL
+dataQuestions2 = sapply(dataQuestions, function(x) paste(strwrap(x, 50), collapse = "\n"))
+names(dataQuestions2) = NULL
 
 # Demographic subsets
-lapop$q2  <- cut(lapop$q2, breaks = c(0,25,35,47,91))
-dataDemos <- names(lapop[,4:7])
+lapop$q2  = cut(lapop$q2, breaks = c(0,25,35,47,91))
+dataDemos = names(lapop[,4:7])
