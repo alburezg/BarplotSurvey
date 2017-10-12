c6271fd280aca1dfbf157038a21e59a8c735439f
Javier Brolo (Javo) <javob@users.noreply.github.com>
2017-10-12 17:11:05 +0300

    v0.2

--- data/app.R
+++ data/app.R
@@ -12,22 +12,22 @@ source("https://raw.githubusercontent.com/javob/BarplotSurvey/master/data/Barplo
 
 
 ui <- fluidPage(
-  headerPanel('Cultura Democrática en Guatemala'),
+  headerPanel('Posiciones ideológicas'),
   sidebarPanel(
-    selectInput("variable", "Variable:",
+    selectInput("variable", "Seleccione una pregunta:",
                 dataQuestions3),
     checkboxGroupInput("q1", "Género:",
-                       c("Hombre" = "1","Mujer" = "2"),
+                       c("Hombre" = 1,"Mujer" = 2),
                        c(1:2)),
     checkboxGroupInput("ur", "Población:",
-                       c("Urbano" = "1","Rural" = "2"),
+                       c("Urbano" = 1,"Rural" = 2),
                        c(1:2)),
-    checkboxGroupInput("etid", "Étnia:",
-                       c("Mestizo" = "1","Indígena" = "2","Otra" = "3"),
+    checkboxGroupInput("etid", "Etnicidad:",
+                       c("Mestiza" = 1,"Indígena" = 2,"Otra" = 3),
                        c(1:3)),
     checkboxGroupInput("q2", "Edad:",
-                       c("De 18 a 25" = "1","De 26 a 35" = "2",
-                         "De 36 a 47" = "3","De 48 a 91" = "4"),
+                       c("De 18 a 25" = 1,"De 26 a 35" = 2,
+                         "De 36 a 47" = 3,"De 48 a 91" = 4),
                        c(1:4))
   ),
   mainPanel(
@@ -37,18 +37,27 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-  selectedData <- reactive({
-    list(q2 = as.numeric(input$q2[1:4]),
-         q1 = as.numeric(input$q1[1:2]),
-         etid = as.numeric(input$etid[1:3]),
-         ur = as.numeric(input$ur[1:2]))
-  })
-  
   mainVar <- reactive({
     dataVars[as.numeric(strsplit(input$variable, "\\.")[[1]][1])]
   })
   
+  selectedData <- reactive({
+    list(q2 = as.numeric(input$q2),
+         q1 = as.numeric(input$q1),
+         etid = as.numeric(input$etid),
+         ur = as.numeric(input$ur))
+  })
+  
   output$plot1 <- renderPlot({
+    
+    validate(
+      need(suppressWarnings(input$q2 != "" & 
+                              input$q1 != "" & 
+                              input$etid != "" & 
+                              input$ur != ""), 
+           "Por favor seleccione al menos una característica." )
+    )
+    
     BarplotSurvey(variable = mainVar(), 
                   selected = selectedData())
   })
